d1a135a791f91bd9ebf43ebe516e32ed1dd9c251
Adam Berenik <adam.berenik@gmail.com>
2020-03-24 19:49:46 +0100

    Odrazanie od stien

--- main.js
+++ main.js
@@ -1,28 +1,37 @@
 var canvas;
 var ctx;
-const POLOMER = 15;
-var posun_x = 0;
-var posun_y = 2*POLOMER;
+const POSUN = 15;
+var posun_x = 2*POSUN;
+var posun_y = 0;
 
 window.onload = function() {
     canvas = document.getElementById("canvas");
     ctx = canvas.getContext("2d");
     
     //innicializacia tela hada
-    for (i = 0; i < 5; i++) {
-        snake.telo.push(new suradnice(100 + i * 2*POLOMER, 100));
+    for (i = 0; i < 50 - 3* POSUN; i++) {
+        snake.telo.push(new suradnice(100 + i * 2*POSUN, 100));
     }
 
     //this.requestAnimationFrame(step);
-    setInterval(step, 1000 /2);
+    setInterval(step, 1000 / 5);
 }
 
 function step() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     snake.draw();
     snake.move();
-    
-    for (i in snake.telo) {
-        console.log(snake.telo[i].x + ' ' + snake.telo[i].y);
+
+    if (snake.telo[0].x >= canvas.width - POSUN || snake.telo[0].x <= 0 + POSUN) {
+        posun_x *= -1;
     }
+    else if (snake.telo[0].y >= canvas.height - POSUN || snake.telo[0].y <= 50 + POSUN) {
+        posun_y *= -1;
+    }
+
+    ctx.beginPath();
+    ctx.moveTo(0, 50);
+    ctx.lineTo(canvas.width, 50);
+    ctx.stroke();
+    ctx.closePath();
 }
--- snake.js
+++ snake.js
@@ -1,13 +1,20 @@
 var snake = {}
 snake.telo = [];
 snake.draw = function() {
-    for (i in this.telo) {
+    for (i = 1; i < this.telo.length-1; i++) {
         ctx.beginPath();
         ctx.arc(this.telo[i].x, this.telo[i].y, 15, 0, 360);
         ctx.closePath();
-        //ctx.fill();
-        ctx.stroke();
+        ctx.fill();
+        //ctx.stroke();
     }
+    //vykreslenie hlavy
+    ctx.fillStyle = "blue";
+    ctx.beginPath();
+    ctx.arc(this.telo[0].x, this.telo[0].y, 15, 0, 360);
+    ctx.closePath();
+    ctx.fill();
+    ctx.fillStyle = "black";
 }
 snake.move = function() {
     //vsetky okrem hlavy sa poposuvaju v poli
@@ -15,6 +22,7 @@ snake.move = function() {
         this.telo[i].x = this.telo[i-1].x;
         this.telo[i].y = this.telo[i-1].y;
     }
+    //hlava sa posunie v smere posunu
     this.telo[0].x += posun_x;
     this.telo[0].y += posun_y;
 }
