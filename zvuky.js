9cd5cc2e88d47ae1c0d2344b3f9bd77eb82a5066
Adam Berenik <adam.berenik@gmail.com>
2020-04-07 20:34:54 +0200

    FIX posledneho commitu

---
+++ zvuky.js
@@ -0,0 +1,13 @@
+var zvuk_gitara;
+var zvuk_nove_jedlo;
+var zvuk_smrt;
+var zvuk_zjedenie;
+
+
+
+function load_sounds() {
+    zvuk_gitara = document.getElementById("gitara");
+    zvuk_nove_jedlo = document.getElementById("nove_jedlo");
+    zvuk_zjedenie = document.getElementById("zjedenie");
+    zvuk_smrt = document.getElementById("smrt");
+}
