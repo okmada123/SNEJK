c95f0f5a1dab3a15f1b35592da921969d4ba9cad
Adam Berenik <adam.berenik@gmail.com>
2020-04-10 21:35:39 +0200

    Vylepsenie zobrazovania skore

--- high_scores.js
+++ high_scores.js
@@ -26,6 +26,7 @@ function high_scores() {
     var zmaz = new Button ("Zmaz historiu", canvas.width - 210, canvas.height - 60, 200, 50);
     zmaz.onclick = function() {
         localStorage.clear();
+        vypis_skore();
     }
     high_scores_scena.clickables.push(zmaz);
 
@@ -34,7 +35,39 @@ function high_scores() {
     }
 
     //vypisanie ulozenych skore
-    if (localStorage.getItem("score_array") === null) alert("Nemas ulozene ziadne skore.");
+    // if (localStorage.getItem("score_array") === null) alert("Nemas ulozene ziadne skore.");
+    // else {
+    //     var pole = JSON.parse(localStorage.getItem("score_array"));
+    //     var y = 50;
+    //     var poradie = 0;
+    //     for (i in pole) {
+    //         //console.log(pole[i]);
+    //         poradie++;
+    //         ctx.save();
+    //         ctx.font = "20px Calibri";
+    //         ctx.textAlign = "center";
+    //         ctx.fillText(poradie + ": " + pole[i][0] + "        \t" + pole[i][1], canvas.width / 2, y);
+    //         ctx.restore();
+
+    //         y += 50;
+    //     }
+    // }
+    vypis_skore();
+}
+
+function vypis_skore() {
+    ctx.save();
+    ctx.fillStyle = "yellow";
+    ctx.fillRect(200, 0, 400, canvas.height);
+    ctx.restore();
+    
+    if (localStorage.getItem("score_array") === null) {
+        ctx.save();
+            ctx.font = "30px Calibri";
+            ctx.textAlign = "center";
+            ctx.fillText("Nemas ulozene ziadne skore.", canvas.width / 2, 300);
+            ctx.restore();
+    }
     else {
         var pole = JSON.parse(localStorage.getItem("score_array"));
         var y = 50;
@@ -47,9 +80,8 @@ function high_scores() {
             ctx.textAlign = "center";
             ctx.fillText(poradie + ": " + pole[i][0] + "        \t" + pole[i][1], canvas.width / 2, y);
             ctx.restore();
-
+            if (poradie >= 10) break;
             y += 50;
         }
     }
-
 }
