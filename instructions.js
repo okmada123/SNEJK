ee7b7125397b2039efd6d04a34478c1a3a627190
Adam Berenik <adam.berenik@gmail.com>
2020-05-06 18:07:00 +0200

    Pridanie popisu tazkej obtiaznosti do instrukcii

--- instructions.js
+++ instructions.js
@@ -22,13 +22,18 @@ function instructions() {
     //vykreslenie instrukcii
     ctx.save();
     ctx.drawImage(logo, canvas.width / 2 - logo.width / 2, 50);
-    ctx.drawImage(sipky, canvas.width / 2 - sipky.width / 2, 400);
     ctx.drawImage(jablko, canvas.width / 5 - jablko.width, 300);
     ctx.drawImage(tabletka, canvas.width - canvas.width / 5, 300);
     ctx.font = "25px Calibri";
     ctx.textAlign = "center";
     ctx.fillText("Jablko +1 dlzka, +10 skore", canvas.width / 5, 250);
     ctx.fillText("Powerup -5 dlzka, +50 skore", canvas.width - canvas.width / 5, 250);
-    ctx.fillText("Pohyb - sipky", canvas.width / 2, 630);
+    ctx.drawImage(sipky, canvas.width / 4 - sipky.width / 2, 400);
+    ctx.fillText("Pohyb - sipky", canvas.width / 4, 630);
+    ctx.fillText("Tazka obtiaznost:", canvas.width - canvas.width / 5, 430);
+    ctx.fillText("Rychlost pohybu sa zvysuje", canvas.width - canvas.width / 5, 500);
+    ctx.fillText("kazdych 10 sekund.", canvas.width - canvas.width / 5, 520);
+    ctx.fillText("Tabletka na skratenie", canvas.width - canvas.width / 5, 560);
+    ctx.fillText("sa vobec neobjavuje.", canvas.width - canvas.width / 5, 580);
     ctx.restore();
 }
